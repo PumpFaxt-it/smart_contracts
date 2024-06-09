31cb3a95bcf2b6f2faa5f63071e388e629a18cd2
Spandan Barve <spandan567@gmail.com>
2024-06-09 21:33:37 +0530

    prettify console output

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,17 +1,25 @@
 import { ethers } from "hardhat";
-import 'dotenv/config'
+import "dotenv/config";
+import { consoleColor } from "../utils";
 
 const ONE_FRAX = BigInt(Math.pow(10, 18));
 
 async function main() {
-  if (!process.env.DEPLOYER_PRIVATE_KEY) throw ("Provide deployer private key")
-  
-  const deployer = new ethers.Wallet(process.env.DEPLOYER_PRIVATE_KEY)
+  if (!process.env.DEPLOYER_PRIVATE_KEY) throw "Provide deployer private key";
+
+  const deployer = new ethers.Wallet(process.env.DEPLOYER_PRIVATE_KEY);
 
   const DummyFrax = await ethers.getContractFactory("DummyFrax");
   const frax = await DummyFrax.deploy();
 
-  console.log("DummyFrax deployed to : ", await frax.getAddress());
+  console.log(
+    consoleColor("yellow"),
+    "DummyFrax ",
+    consoleColor("white"),
+    "deployed to : ",
+    consoleColor("cyan"),
+    await frax.getAddress()
+  );
 
   const PumpItFaxtInterface = await ethers.getContractFactory(
     "PumpItFaxtInterface"
@@ -23,9 +31,15 @@ async function main() {
   pumpItFaxt.setMaximumInitialTokenSupply(BigInt(1_000_000_000) * ONE_FRAX);
 
   console.log(
-    "PumpItFaxtInterface deployed to : ",
+    consoleColor("yellow"),
+    "PumpItFaxtInterface ",
+    consoleColor("white"),
+    "deployed to : ",
+    consoleColor("cyan"),
     await pumpItFaxt.getAddress()
   );
+
+  console.log(consoleColor("white"));
 }
 
 main()
--- utils.ts
+++ utils.ts
@@ -2,3 +2,18 @@ export function checkSorted(arr: any[]) {
   const sortArr = [...arr].sort((a, b) => a - b);
   return JSON.stringify(arr) === JSON.stringify(sortArr);
 }
+
+const consoleColors = {
+  black: "\x1b[30m",
+  red: "\x1b[31m",
+  green: "\x1b[32m",
+  yellow: "\x1b[33m",
+  blue: "\x1b[34m",
+  magenta: "\x1b[35m",
+  cyan: "\x1b[36m",
+  white: "\x1b[37m",
+};
+
+export function consoleColor(clr: keyof typeof consoleColors) {
+  return consoleColors[clr];
+}
